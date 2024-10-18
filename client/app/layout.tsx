aaa07dc6004bb97c166fcb7308d61fafae8fc909
Cihan <officialcihan0248@gmail.com>
2024-10-19 00:33:16 +0300

    icon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { SidebarLayout } from "../components/sidebar-layout";
 import "./globals.css";
+import logo_bird from "../public/origami_2722241.png";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -11,7 +12,7 @@ const font = Poppins({
 
 export const metadata: Metadata = {
   title: "Chain's Dev Blog",
-  description: "Exploring the world of code, one commit at a time.",
+  description: "Exploring the world of myself.",
 };
 
 export default function RootLayout({
@@ -21,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <link rel="icon" href={logo_bird.src} />
+      </head>
       <body className={`${font.className} h-full`}>
         <div className="h-full w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] relative flex">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black"></div>
