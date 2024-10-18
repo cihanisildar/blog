6bd95ea67515d567d71cc2ab74b04a65d115d33e
Cihan <officialcihan0248@gmail.com>
2024-10-18 23:59:41 +0300

    cors2

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,8 @@ const app = express();
 const allowedOrigins = [
   process.env.FRONTEND_URL1,
   process.env.FRONTEND_URL2,
+  "https://blog-omega-ten-84.vercel.app",
+  "https://blogclient-ten.vercel.app",
   "http://localhost:3002"
 ].filter(Boolean);
 
