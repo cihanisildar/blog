2642b194ced57d08c06fcdf1799b28421f402aa1
Cihan <officialcihan0248@gmail.com>
2024-10-18 16:58:15 +0300

    middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,8 +1,18 @@
+// middleware.ts
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  // console.log('Request received:', req);
+  // Your middleware logic here, e.g., checking authentication, modifying headers, etc.
+
+  // You can return a response if needed
+  return NextResponse.next(); // Continue to the next middleware or request handler
+}
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
